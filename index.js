e8382d894c9a93f852f3214d3784c57555010799
zziuni <zziuni@gmail.com>
2013-07-13 01:48:26 +0900

    add prepared info with colorfull

--- index.js
+++ index.js
@@ -1,39 +1,89 @@
-var options = {useGlobal:true}
+//module.exports = require("./lib/repl");
+
+var replD = require("repl")
+    , fs = require("fs")
+    , colors = require('colors')
+    , logErrors = require('log-errors')
+    , _ = require("lodash")
+    , $ = require("jquery")
+    , d3 = require("d3")
+    , dependencies = []
+    , devDependencies = []
+    , packageFile = null
+
+var options = {
+        useGlobal: true,
+        prompt: "Dev >",
+        ignoreUndefined: false
+    }
     , alias = {
         "jquery": "$"
     }
+    , logProd = logErrors.production
+    , logDev = logErrors.development;
 
-var replD = require("repl").start( options )
-    , fs = require("fs")
-    , _ = require("lodash")
-    , depandencies, devDepandencies;
+try{
+    packageFile = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+    dependencies = packageFile.dependencies || {};
+    devDependencies = packageFile.devDependencies || {};
+}catch(e){
+    logDev(e);
+    process.exit();
+}
 
-var package = require("./package.json");
+//var iterator = [dependencies, devDependencies];
+var iterator = dependencies;
+var modules = [];
+var message = [];
 
-dependencies = package.dependencies || {};
-devDependencies = package.devDependencies || {};
+function getSuccessMessage(){
 
-var iterator = [dependencies, devDependencies];
-var successedModule = [];
+}
 
-console.log( '===============================' );
+function getFailureMessage(){
 
-_( iterator ).forEach( function( deps ){
-    successedModule = _( deps ).filter( function( versionString, moduleName ){
-        try{
-            var module = alias[moduleName ] || moduleName
-            replD.context[ module  ] = require( moduleName );
-            console.log( "'" + moduleName + "' " + versionString + " loaded.");
-        }catch(e){
-            console.log( "'" + moduleName + "' " + versionString + " can't loaded.");
-        }
-    })
+}
+
+function setMessage( isSuccess, info){
+    var msg = null;
+    if( isSuccess ){
+        msg = colors.green(info.depName ).bold + "(" + info.version + ") module loaded."
+    }else{
+        msg = colors.red(info.depName ).bold + "(" + info.version + ") module can't loaded. please npm install"
+    }
+    message.push(msg)
+}
+
+//generate module.
+_( iterator ).forEach( function(  version, depName  ){
+    var isSuccess = false;
+
+    try{
+        modules.push( {
+            name: depName,
+            module: require( depName )
+        } );
+        isSuccess = true;
+    }catch(e){ }
+
+    setMessage( isSuccess, {version: version, depName: depName} );
 });
 
-console.log( '===============================' );
+//print message
+console.log( '\nLoading modules by package.json'.bold.inverse );
+_(message).forEach( function( msg ){
+    console.log( msg );
+});
+console.log( 'Complete loading \n'.bold.inverse  );
+
+//Create REPL
+replD = replD.start( options );
+
+_( modules ).forEach( function( module ){
+    var name = alias[module.name ] || module.name;
+    replD.context[ name ] = module.module;
+});
 
 replD.on("exit", function(){
-    console.log( '===============================' );
-    console.log("replD is exiting, Good by~");
-    console.log( '===============================' );
+    console.log("Good by".bold.inverse);
 });
